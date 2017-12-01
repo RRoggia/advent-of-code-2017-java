0600d8950262aea93c4cdcc59e290c480b42b532
RRoggia <renanzr@gmail.com>
2017-12-01 20:59:12 -0200

    day one first part of the challenge

---
+++ src/com/rroggia/day1/CaptchaSolver.java
@@ -0,0 +1,44 @@
+package com.rroggia.day1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class CaptchaSolver {
+
+	public static void main(String[] args) throws IOException {
+
+		Scanner scanner = null;
+
+		try {
+			scanner = new Scanner(new BufferedReader(new FileReader("./src/com/rroggia/day1/puzzleInput")));
+			String puzzleInput = scanner.next();
+
+			int firstValue;
+			int secondValue;
+			int sum = 0;
+
+			for (int i = 0; i < puzzleInput.length(); i++) {
+				firstValue = Character.getNumericValue(puzzleInput.charAt(i));
+				secondValue = Character.getNumericValue(puzzleInput.charAt(getNextIndex(puzzleInput, i)));
+
+				if (firstValue == secondValue) {
+					sum += firstValue;
+				}
+			}
+
+			System.out.println(sum);
+
+		} finally {
+			if (scanner != null) {
+				scanner.close();
+			}
+		}
+	}
+
+	private static int getNextIndex(String string, int currentIndex) {
+		return string.length() - 1 == currentIndex ? 0 : currentIndex + 1;
+	}
+
+}
